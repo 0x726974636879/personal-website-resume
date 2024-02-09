d466c7c3c12e9cd51a2a8fa6451afab29e217ca9
monsieur_r <monsieur_r@monsieur-rs-MacBook-Air.local>
2024-02-09 17:04:05 +0100

    refacto

--- src/components/panels/LeftPanel.tsx
+++ src/components/panels/LeftPanel.tsx
@@ -1,25 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './styles/LeftPanel.module.css';
+import photo from '../../assets/photo.png';
 
 const LeftPanel: React.FC = () => {
+  const colors = ['#ffffff', '#0a0a0a', '#1abc9c', '#2ecc71', '#3498db', '#9b59b6', '#9b59b6', '#34495e', '#7A0707'];
+  const [colorIndex, setColorIndex] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setColorIndex((prevIndex) => (prevIndex + 1) % colors.length);
+    }, 2000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const backgroundColor = colors[colorIndex];
+
   return (
-    <section className="flex-1 flex flex-col items-center justify-center w-1/2">
-      <div>
-        <h1>Hello i'm Ritchy</h1>
+    <section
+      className={`${styles.leftPanel} flex-1 flex flex-col items-center`}
+      style={{ backgroundColor }}
+    >
+      <div className="flex-1 flex flex-col items-center justify-center">
+        <img className="max-w-[60%]" src={photo} title="First, solve the problem. Then, write the code."></img>
+        <h1 className="text-[2rem]">Hello i'm Ritchy</h1>
         <p>a developer</p>
-        <p>i'm currently learning</p>
-        <div className="learning-icon"></div>
+        <p className="mt-6 text-[2rem]">i'm currently learning</p>
+        <div className={`${styles.learningIcon} flex w-full justify-evenly`}>
+          <img className="m-6" src="https://upload.wikimedia.org/wikipedia/commons/4/4c/Typescript_logo_2020.svg" alt="Typescript" title="Typescript"></img>
+          <img className="m-6" src="https://upload.wikimedia.org/wikipedia/commons/3/39/Kubernetes_logo_without_workmark.svg" alt="Kubernetes" title="Kubernetes"></img>
+        </div>
         <p>My certifications</p>
-        <div className={`${styles.certifications} flex`}>
+        <div className={`${styles.certifications} flex justify-evenly w-full`}>
           <img src="https://d1.awsstatic.com/training-and-certification/certification-badges/AWS-Certified-Solutions-Architect-Associate_badge.3419559c682629072f1eb968d59dea0741772c0f.png" alt="Badge AWS Certified Solutions Architect - Associate" title="Badge AWS Certified Solutions Architect - Associate"></img>
           <img src="https://d1.awsstatic.com/training-and-certification/certification-badges/AWS-Certified-Developer-Associate_badge.5c083fa855fe82c1cf2d0c8b883c265ec72a17c0.png" alt="AWS Certified Developer - Associate badge" title="Badge AWS Certified Developer - Associate"></img>
           <img src="https://d1.awsstatic.com/training-and-certification/certification-badges/AWS-Certified-SysOps-Administrator-Associate_badge.c3586b02748654fb588633314dd66a1d6841893b.png" alt="Badge pour AWS Certified SysOps Administrator - Associate" title="Badge AWS Certified SysOps - Associate"></img>
           <img src="https://d1.awsstatic.com/certification/badges/AWS-Certified-Cloud-Practitioner_badge_150x150.17da917fbddc5383838d9f8209d2030c8d99f31e.png" alt="Badge AWS Certified Cloud Practitioner" title="Badge AWS Certified Cloud Practitioner"></img>
           <img src="https://www.datocms-assets.com/2885/1645553469-hcta0-badge.png" alt="Badge HashiCorp Certified Terraform - Associate"></img>
         </div>
-        <h2>Let's work together...</h2>
-        <p>How do you take your coffee?</p>
+        <h2 className="text-[2rem]">Let's work together...</h2>
+        <a href="">How do you take your coffee?</a>
       </div>
+
     </section>
   );
 }
--- src/components/panels/RightPanel.tsx
+++ src/components/panels/RightPanel.tsx
@@ -3,66 +3,66 @@ import styles from './styles/RightPanel.module.css';
 
 
 const RightPanel: React.FC = () => {
-    // Simulated Python code for demonstration purposes
-    useEffect(() => {
-        if (window.Prism) {
-            window.Prism.highlightAll();
-        }
-    }, []);
+	// Simulated Python code for demonstration purposes
+	useEffect(() => {
+			if (window.Prism) {
+					window.Prism.highlightAll();
+			}
+	}, []);
 
-    const pythonCode = `
+	const pythonCode = `
 from typing import Dict, List
 
 
 class Dev:
-    def __init__(self):
-        self.name: str = "Ritchy B."
-        self.job: str = "Cloud Developer"
-        self.email: str = "contact@portefine.fr"
-        self.location: str = "Paris - France"
+	def __init__(self):
+		self.name: str = "Ritchy B."
+		self.job: str = "Cloud Developer"
+		self.email: str = "contact@portefine.fr"
+		self.location: str = "Paris - France"
 
-        self.experiences: Dict[str, str] = {
-            "2023": "Cloud Consultant @ Devoteam Revolve, .....",
-            "2023": "SysOps @ Veolia Water Technologies, .....",
-            "2021": "CloudOps @ Jabmo, .....",
-            "2018": "CloudOps @ Lineup 7, ....."
-        }
+		self.experiences: Dict[str, str] = {
+			"2023": "Cloud Consultant @ Devoteam Revolve",
+			"2023": "SysOps @ Veolia Water Technologies",
+			"2021-2023": "CloudOps @ Jabmo",
+			"2018-2021": "CloudOps @ Lineup 7"
+		}
 
-        self.studies: Dict[str, str] = {
-            "2017": "3W Academy",
-            "2016": "Self-taught",
-            "2015": "42"
-        }
+		self.studies: Dict[str, str] = {
+			"2017": "3W Academy",
+			"2016": "Self-taught",
+			"2015": "42"
+		}
 
-        self.realisations: List[str] = []
+		self.realisations: List[str] = []
 
-        self.coding_knowledge: List[str] = [
-            "Python",
-            "Terraform",
-            "Amazon Web Services",
-            "Javascript",
-            "SQL",
-            "Github",
-            "Event-driven Architecture",
-            "SOLID design principles"
-        ]
-        self.coding_framework: List[str] = [
-            "Django", "FastAPI", "Flask", "Pygame"
-        ]
-        self.softwares: List[str] = ["VSCode", "Unix", "Linux", "MacOS"]
+		self.coding_knowledge: List[str] = [
+			"Amazon Web Services",
+			"Python",
+			"Git",
+			"Terraform",
+			"Event-driven Architecture",
+			"SOLID design principles",
+			"Javascript",
+			"SQL"
+		]
+		self.coding_framework: List[str] = [
+			"Django", "Flask", "FastAPI", "Pygame"
+		]
+		self.softwares: List[str] = ["VSCode", "Unix", "Linux", "MacOS"]
 
-        self.links: Dict[str, str] = {
-            "website": "seriously ? you are already there.",
-            "github": "github.com/0x726974636879",
-            "linkedin": "linkedin.com/in/ritchyb",
-            "freelance": "malt.fr/ritchyblezin"
-        }
+		self.links: Dict[str, str] = {
+			"website": "seriously ? you are already there.",
+			"github": "github.com/0x726974636879",
+			"linkedin": "linkedin.com/in/ritchyb",
+			"freelance": "malt.fr/ritchyblezin"
+		}
 `;
 
   return (
-    <section className={`${styles.rightPanel} flex-2 w-1/2`}>
+    <section className={`${styles.rightPanel} flex-2 w-1/2 text-sm`}>
         <pre>
-            <code className="language-python">{pythonCode}</code>
+			    <code className="language-python">{pythonCode}</code>
         </pre>
     </section>
   );
